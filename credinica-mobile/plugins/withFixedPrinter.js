d1977ea574297bbca5f56a2efc935b6f704da50a
harwin221 <harwinrueda221@gmail.com>
2026-04-06 03:19:53 -0600

    Final Build: High-level Gradle shielding for Insecure Protocols

--- credinica-mobile/plugins/withFixedPrinter.js
+++ credinica-mobile/plugins/withFixedPrinter.js
@@ -1,16 +1,35 @@
 const { withProjectBuildGradle } = require('@expo/config-plugins');
 
 /**
- * Expo Config Plugin to fix the 'insecure protocol' and JCenter issues
- * for the react-native-bluetooth-escpos-printer library.
+ * Expo Config Plugin to eliminate 'insecure protocol' and JCenter issues
+ * for the legacy bluetooth printer library in Gradle 8+.
  */
 const withFixedPrinter = (config) => {
   return withProjectBuildGradle(config, (config) => {
-    // 1. Force allowInsecureProtocol for anything that might still use http
-    if (!config.modResults.contents.includes('allowInsecureProtocol')) {
-        config.modResults.contents = config.modResults.contents.replace(
-          /allprojects\s*{[\s\S]*?repositories\s*{/,
-          `allprojects {
+    let contents = config.modResults.contents;
+
+    // 1. Force secure protocols and replace jcenter in ALL repositories blocks
+    contents = contents.replace(/jcenter\(\)/g, 'mavenCentral()');
+    contents = contents.replace(/maven\s*{\s*url\s*["']http:\/\/jcenter.bintray.com\/["']\s*}/g, 'mavenCentral()');
+
+    // 2. Global opt-in for insecure protocols (Nuclear option to bypass all errors)
+    if (!contents.includes('allowInsecureProtocol = true')) {
+        contents = contents.replace(
+            /allprojects\s*{[\s\S]*?repositories\s*{/,
+            `allprojects {
+    repositories {
+        all {
+            if (it instanceof MavenArtifactRepository && it.url.toString().startsWith("http:")) {
+                it.allowInsecureProtocol = true
+            }
+        }
+        `
+        );
+        
+        // Also apply to buildscript
+        contents = contents.replace(
+            /buildscript\s*{[\s\S]*?repositories\s*{/,
+            `buildscript {
     repositories {
         all {
             if (it instanceof MavenArtifactRepository && it.url.toString().startsWith("http:")) {
@@ -21,9 +40,7 @@ const withFixedPrinter = (config) => {
         );
     }
 
-    // 2. Replace JCenter with Central if it appears
-    config.modResults.contents = config.modResults.contents.replace(/jcenter\(\)/g, 'mavenCentral()');
-    
+    config.modResults.contents = contents;
     return config;
   });
 };
